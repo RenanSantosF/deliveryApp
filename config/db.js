7e83be4a9b5d8eaa4c4fe1d968f09220d6efe480
Renan Santos <renan_ferreira.es@outlook.com>
2024-05-27 22:10:56 -0300

    Altera URI

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == "production") {
-  module.exports = { mongoURI: "mongodb+srv://renanSantosF:bAPjXNANrmLQwjvb@menu.hkhlajn.mongodb.net/?retryWrites=true&w=majority&appName=menu" };
+  module.exports = { mongoURI: "mongodb+srv://renanSantosF:bAPjXNANrmLQwjvb@menu.hkhlajn.mongodb.net/?retryWrites=true&w=majority&ssl=true" };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/Menu" };
 }
